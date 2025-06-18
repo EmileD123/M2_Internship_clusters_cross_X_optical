26b4e542bfc040ecd6520aff2a48aae12933881a
DOSSO Emile <ED282972@extra.cea.fr>
2025-06-18 17:14:53 +0200

    propagation erreurs avec optim + premiers calculs de nombre d'amas

--- caracterisation selection effects/halo_and_cluster_MF.py
+++ caracterisation selection effects/halo_and_cluster_MF.py
@@ -19,6 +19,7 @@ def HMF_MRP(log10_Mmedian_Msun,log10_phimedian,alpha,beta):
         # log10_M_Msun : log10(M/M_sun)
         log10_M_Msun = np.array(log10_M_Msun) # Assure que c'est un tableau numpy
         M_Msun = 10**(log10_M_Msun)
+        
         phimedian = 10**(log10_phimedian)
         Mmedian_Msun = 10**(log10_Mmedian_Msun)
 
@@ -35,9 +36,42 @@ def CMF_MRP(log10_Mmedian_Msun,log10_phimedian,alpha,beta):
         # log10_M_Msun : log10(M/M_sun)
 
         HMF = HMF_MRP(log10_Mmedian_Msun,log10_phimedian,alpha,beta)
-        result = HMF(log10_M_Msun) * (1/0.85) # On considère que la masse d'un amas de galaxie provient à 85% de la matière noire (le reste étant 10% de gaz ICM et 5% de galaxies)
+        result = HMF(log10_M_Msun) * (1/0.85) # On considère que la masse d'un amas de galaxies provient à 85% de la matière noire (le reste étant 10% de gaz ICM et 5% de galaxies)
         return result
     
     return CMF_MRP_result
+
+
+def CMF_MRP_2(log10_M_Msun): 
+    # L'intérêt de cette fonction est de pouvoir optimiser la valeur de fonction de masse des amas pour une masse fixée et donc en fonction des paramètres fittés
+    def CMF_MRP_2_result(params): # params est un tableau de 4 paramètres : log10_Mmedian_Msun, log10_phimedian, alpha, beta
+        M_Msun = 10**(log10_M_Msun)
+
+
+        log10_Mmedian_Msun = params[0]; log10_phimedian = params[1] ; alpha = params[2] ; beta = params[3]
+        log10_Mmedian_Msun = np.array(log10_Mmedian_Msun)  ; Mmedian_Msun = 10**(log10_Mmedian_Msun)
+        log10_phimedian = np.array(log10_phimedian) ; phimedian = 10**(log10_phimedian)
+        alpha = np.array(alpha)
+        beta = np.array(beta)
+
+        result = np.log(10) * phimedian * beta * ((M_Msun * (Mmedian_Msun)**(-1))**(alpha+1)) * np.exp(-((M_Msun * (Mmedian_Msun)**(-1))**(beta))) * (1/0.85) # * 1/0.85 car on considère que la DM représente 85% de la masse d'un cluster
+        return result
+    return CMF_MRP_2_result
+
+def CMF_MRP_2_opposite(log10_M_Msun): 
+    # Même fonction que CMF_MRP_2 mais avec résultat opposé pour maximiser en utilisant la fonction minimize de scipy.optimize
+    def CMF_MRP_2_opposite_result(params): # params est un tableau de 4 paramètres : log10_Mmedian_Msun, log10_phimedian, alpha, beta
+        M_Msun = 10**(log10_M_Msun)
+
+
+        log10_Mmedian_Msun = params[0]; log10_phimedian = params[1] ; alpha = params[2] ; beta = params[3]
+        log10_Mmedian_Msun = np.array(log10_Mmedian_Msun)  ; Mmedian_Msun = 10**(log10_Mmedian_Msun)
+        log10_phimedian = np.array(log10_phimedian) ; phimedian = 10**(log10_phimedian)
+        alpha = np.array(alpha)
+        beta = np.array(beta)
+
+        result = (-1) * np.log(10) * phimedian * beta * ((M_Msun * (Mmedian_Msun)**(-1))**(alpha+1)) * np.exp(-((M_Msun * (Mmedian_Msun)**(-1))**(beta))) * (1/0.85) # * 1/0.85 car on considère que la DM représente 85% de la masse d'un cluster
+        return result
+    return CMF_MRP_2_opposite_result
 
 
